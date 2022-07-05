822aa683c29d894caf829ca2ffb336c8331431c5
hanieith <shkutkoff@gmail.com>
2022-07-05 12:27:41 +0300

    04.07

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
-from flask import render_template, Flask, request, g
+from flask import render_template, Flask, request, g, flash, abort
+from FDataBase import FDataBase
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -27,7 +28,7 @@ def connect_db():
 
 
 def get_db():
-    '''Соединение с БД, если оно еще не установлено'''
+    # '''Соединение с БД, если оно еще не установлено'''
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
@@ -35,9 +36,45 @@ def get_db():
 
 @app.teardown_appcontext
 def close_db(error):
-    '''Закрываем соединение с БД, если оно было установлено'''
+    # '''Закрываем соединение с БД, если оно было установлено'''
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 if __name__ == "__main__":
--- sq_db.sql
+++ sq_db.sql
@@ -3,3 +3,10 @@ CREATE TABLE IF NOT EXISTS mainmenu (
     title text not null,
     url text not null
 );
+
+CREATE TABLE IF NOT EXISTS posts (
+id integer PRIMARY KEY AUTOINCREMENT,
+title text NOT NULL,
+text text NOT NULL,
+time integer NOT NULL
+);
