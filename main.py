61dc468389c899a97454b71ab901d11a5ca86ea5
hanieith <shkutkoff@gmail.com>
2022-07-07 14:47:11 +0300

    07.07

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sqlite3
 import os
-from flask import render_template, Flask, request, g, flash, abort
+from flask import render_template, Flask, request, g, flash, abort, redirect, session, url_for
 from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import *
+from UserLogin import UserLogin
+
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -10,7 +14,21 @@ SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+login_manager = LoginManager(app)
+dbase = None
 
+
+@app.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return UserLogin().fromDB(user_id, dbase)
 
 def create_db():
     """Вспомогательная функция для создания таблиц БД"""
@@ -67,6 +85,7 @@ def addPost():
 
 
 @app.route("/post/<int:id_post>")
+@login_required
 def showPost(id_post):
     db = get_db()
     dbase = FDataBase(db)
@@ -75,6 +94,39 @@ def showPost(id_post):
         abort(404)
 
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = dbase.getUserByEmail(request.form['email'])
+        if user and check_password_hash(user['psw'], request.form['psw']):
+            userlogin = UserLogin().create(user)
+            login_user(userlogin)
+            return redirect(url_for('index'))
+
+        flash("Неверная пара логин/пароль", "error")
+
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
+
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        session.pop('_flashes', None)
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+                and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
+    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
 
 
 if __name__ == "__main__":
--- sq_db.sql
+++ sq_db.sql
@@ -10,3 +10,11 @@ title text NOT NULL,
 text text NOT NULL,
 time integer NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS users (
+id integer PRIMARY KEY AUTOINCREMENT,
+name text NOT NULL,
+email text NOT NULL,
+psw text NOT NULL,
+time integer NOT NULL
+);
